ce156c2f7399719fa00a4c6753944cca7576cd8f
Isac Zobec <zobec.isac@gmail.com>
2025-10-09 22:28:59 +0200

    begin adding parse tree builder

--- src/syntax_rules.cpp
+++ src/syntax_rules.cpp
@@ -1,4 +1,5 @@
 #include "syntax_rules.h"
+#include <stdexcept>
 
 namespace ParseTree {
 
@@ -15,11 +16,12 @@ namespace ParseTree {
             possibleDefinitions.back().reserve(INITIAL_DEFINITION_COMPONENT_CAPACITY);
         }
     }
-
+    
+    Rule::Rule() {}
     Rule::Rule(size_t definitionAmount) {
         CreateDefinitionsUpTo(definitionAmount-1);
     }
-
+        
     void Rule::AddRuleComponent(size_t definitionIndex, Rule* rule) {
         CreateDefinitionsUpTo(definitionIndex);
         possibleDefinitions[definitionIndex].emplace_back(rule);
@@ -29,4 +31,33 @@ namespace ParseTree {
         CreateDefinitionsUpTo(definitionIndex);
         possibleDefinitions[definitionIndex].emplace_back(token);
     }   
+
+    Rule* ParseTreeBuilder::CreateRule(size_t ruleIndex, size_t definitionAmount) {
+        if (ruleIndex < 0 || ruleIndex >= RULE_AMOUNT) {
+            throw std::logic_error("Rule index was out of bounds.");
+            return;
+        }
+        rules[ruleIndex] = Rule(definitionAmount);
+    }
+
+    Rule* ParseTreeBuilder::GetRule(size_t ruleIndex) {
+        if (ruleIndex < 0 || ruleIndex >= RULE_AMOUNT) {
+            throw std::logic_error("Rule index was out of bounds.");
+            return;
+        }
+        return &rules[ruleIndex];
+    }
+
+    ParseTreeBuilder::ParseTreeBuilder() {
+        CreateRules();
+    }
+
+    void ParseTreeBuilder::CreateRules() {
+
+        Rule* testRule = CreateRule(0, 2);
+        testRule->AddRuleComponent(0, TokenType::BRACKET);
+        testRule->AddRuleComponent(0, TokenType::BRACKET);
+    }
+
+
 }
--- src/syntax_rules.h
+++ src/syntax_rules.h
@@ -8,6 +8,8 @@ namespace ParseTree {
 
     inline constexpr TokenType RULECOMPONENT_NO_TOKEN = TokenType::NONE;
     inline constexpr Rule* RULECOMPONENT_NO_RULE = NULL;
+
+    inline constexpr int RULE_AMOUNT = 2;
     
     /* The amount of `DefinitionComponent`s a definition by default gets allocated space for. */
     inline constexpr int INITIAL_DEFINITION_COMPONENT_CAPACITY = 4;
@@ -28,12 +30,25 @@ namespace ParseTree {
 
         public:
         Rule(size_t definitionAmount);
+        Rule();
 
         void AddRuleComponent(size_t definitionIndex, Rule* rule);
         void AddRuleComponent(size_t definitionIndex, TokenType token);
     };  
 
     class ParseTreeBuilder {
-        
+        private:
+        Rule rules[RULE_AMOUNT];
+
+        void CreateRules();
+
+        public:
+        ParseTreeBuilder();
+        ~ParseTreeBuilder();
+
+        Rule* CreateRule(size_t ruleIndex, size_t definitionAmount);
+        Rule* GetRule(size_t ruleIndex);
+
+
     };
 }
